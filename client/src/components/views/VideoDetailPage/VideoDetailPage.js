754aaed29365124e0e3c194130d130def97b6c00
JinHawn <b980131@gmail.com>
2021-11-30 16:48:52 +0900

    finished

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,6 +4,7 @@ import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
 import Comment from "./Sections/Comment";
+import LikeDislike from "./Sections/LikeDislike";
 
 function VideoDetailPage(props) {
   const videoId = props.match.parames.videoId; //Video아이디를 가져오는 것이다.
@@ -51,7 +52,16 @@ function VideoDetailPage(props) {
               src={`http://localhost:5000/${VideoDetail.filePath}`}
               controls
             />
-            <List.Item actions={[subscribeButton]}>
+            <List.Item
+              actions={[
+                <LikeDislikes
+                  video
+                  userId={localStorage.getItem("userId")}
+                  videoId={videoId}
+                />,
+                subscribeButton,
+              ]}
+            >
               <List.Item.Meta
                 avatar={<Avatar src={VideoDetail.writer.image} />}
                 title={VideoDetail.writer.name}
---
+++ server/models/Dislike.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dislikeSchema = mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    commentId: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    videoId: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  },
+  { timestamps: true } //만든 날짜와 업데이트한 날짜가 표시가된다.
+);
+
+const Dislike = mongoose.model("Dislike", videoSchema);
+
+module.exports = { Dislike };
